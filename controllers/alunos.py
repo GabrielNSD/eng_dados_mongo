7abaff42b9d07c5ebe9e42306042fa3bc2961829
Micael Balza <54646857+micaelbalza@users.noreply.github.com>
2022-12-12 18:08:38 -0300

    return

--- controllers/alunos.py
+++ controllers/alunos.py
@@ -5,9 +5,17 @@ client = get_client()
 db = client.ufrn_database
 collection = db.alunos
 
+'''
+Retorna uma lista de items que possuem o campo com valor indicado na função
+'''
+
 
 def filtrar_alunos(campo, valor):
     return list(collection.find({campo: valor}, {'_id': False}))
+
+'''
+Pesquisa multi campo, recebe um dicionário como argumento. Esse dicionário contem os campos e seus respectivos valores a serem buscados
+'''
 
 
 def filtrar_alunos_multi_campo(search_dict):
